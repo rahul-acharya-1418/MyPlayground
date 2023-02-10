8b3b5e8d43a60e44a06745b5766cc94ad4e12c6d
rahul-acharya-1418 <acharyar00071@gmail.com>
2023-02-10 13:08:35 +0530

    add BetterOptional

--- MyPlayground.playground/Pages/BetterOptional.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/BetterOptional.xcplaygroundpage/Contents.swift
@@ -1,2 +1,9 @@
 import Foundation
 
+let multilineString = """
+1st line
+2nd line
+3rd line
+"""
+
+print(multilineString)
--- MyPlayground.playground/Pages/String.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/String.xcplaygroundpage/Contents.swift
@@ -2,7 +2,8 @@ import Foundation
 /*
  
 1. Convert String to array with space
- 
+2. raw string
+3. isNilOrEmpty
 */
 
 // 1. Convert String to array with space
@@ -13,7 +14,33 @@ print(nameComponent)
 let firstName = nameComponent[0]
 print(firstName)
 
+// 2. raw string
+// let json = "{\"name\":\"Vincent\"}"
+let json = #"{"name":"Vincent"}"#
+print(json)
+
+
+// 3. isNilOrEmpty
+extension Optional where Wrapped == String {
+    var isNilOrEmpty : Bool {
+        return self == nil || self == ""
+    }
+}
+
+func validateUser(name: String) {
+    if name != nil, name != "" {
+// instead of
+//        if !name.isNilOrEmpty {
 //
+//        }
+    }
+}
 
-
-
+// 4. Multiline
+// let multiline = "1st line \n2nd line \n3rd line"
+// instead of
+let multilineString = """
+1st line
+2nd line
+3rd line
+"""
